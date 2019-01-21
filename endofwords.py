d83e14519db332b429d366a5384dd546f878f366
George Tempel <gtempel@billtrust.com>
2019-01-21 15:29:27 -0500

    added scripts from the mini-course

---
+++ endofwords.py
@@ -0,0 +1,12 @@
+word1 = 'Good'
+end1 = word1[2] + word1[3]
+end1 = word1[2:4]
+end1 = word1[2:]
+half1 = len(word1) // 2
+end1 = word1[half1:]
+
+word2 = 'Evening'
+end2 = word2[3:]
+half2 = len(word2) // 2
+end2 = word2[half2:]
+print(end1, end2)
---
+++ montypython.py
@@ -0,0 +1,23 @@
+# Describe the sketch comedy group
+
+first_name = 'Monty'
+last_name = 'Python'
+
+print(first_name, last_name)
+
+name = first_name + ' ' + last_name
+print(name)
+
+description = 'sketch comedy group'
+year = 1969
+
+
+# introduce them
+sentence = name + ' is a ' + description + ' formed in ' + str(year)
+print(sentence)
+print(name, 'is a', description, 'formed in', year)
+
+famous_sketch1 = "\n\tHell's Grannies"
+famous_sketch2 = '\n\tThe Dead Parrot'
+print('Famous Work:', famous_sketch1, famous_sketch2)
+
