27a268260a39fa892f71693d0f1f08ab4df0a4df
George Tempel <gtempel@billtrust.com>
2019-03-20 10:51:30 -0400

    enhanced comprehension examples

--- comprehensionstuff.py
+++ comprehensionstuff.py
@@ -29,6 +29,32 @@ def find_all_primes_less_than_or_equal_to(n):
     print(f"Primes <= {n}:", results)
 
 
+def comprehensions_with_multiple_sequences(sequence_a, sequence_b):
+    # read this like nested for loops, where the comprehension is
+    # "executed" within the innermost/rightmost for loop
+    #
+    # the results container is created atomically -- it works or doesn't
+    results = [(x,y) for x in sequence_a for y in sequence_b]
+    print(f"comprehensions from multiple sequences: ", results)
+
+
+def comprehensions_with_multiple_if_clauses_and_multiple_sequences(sequence_a, sequence_b):
+    # read this like nested for loops, with the if clauses within the
+    # respective loop(s)
+    # results = [ x / (x - y) for x in sequence_a if x > 50 for y in sequence_b if x-y != 0]
+    results = [ x / (x - y) 
+        for x in sequence_a 
+        if x > 50 
+        for y in sequence_b 
+        if x-y != 0]
+    print(f"comprehensions from multiple if clauses and multiple sequences: ", results)
+
+
+def nested_comprehensions(sequence_a):
+    results = [[ y * 3 for y in range(x)] for x in sequence_a]
+    print(f"nested comprehensions: ", results)
+
+
 def main():
     length_of_first_n_factorials(20)
 
@@ -42,6 +68,10 @@ def main():
     dictionary_comprehension_to_swap_keys_and_values(country_to_capital)
 
     find_all_primes_less_than_or_equal_to(101)
+
+    comprehensions_with_multiple_sequences(range(3), range(4))
+    comprehensions_with_multiple_if_clauses_and_multiple_sequences(range(100), range(100))
+    nested_comprehensions(range(10))
 
 
 if __name__ == '__main__':
